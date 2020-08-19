dda98e76dbf2751167ead2b41e9e43a981dce563
Gurkirat Singh <gurkiratsinghofficial@gmail.com>
2020-08-19 17:39:28 +0530

    bg color change

--- src/components/Navb.js
+++ src/components/Navb.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import {useState,useEffect} from 'react'
 import {Navbar,Form,Nav} from 'react-bootstrap'
 
+const useThemeDetector = () => {
+  const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDarkTheme, setIsDarkTheme] = useState(getCurrentTheme());  
+  const mqListener = (e => {
+      setIsDarkTheme(e.matches);
+  });
+  
+  useEffect(() => {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    darkThemeMq.addListener(mqListener);
+    return () => darkThemeMq.removeListener(mqListener);
+  }, []);
+  return isDarkTheme;
+}
 function Navb() {
+  const isDarkTheme = useThemeDetector();
     return (
         <div>
-            <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar bg={isDarkTheme ? "dark": "light"} variant={isDarkTheme ? "dark": "light"} expand="lg">
               <Navbar.Brand href="#home">GURKIRAT SINGH</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
