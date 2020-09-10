b76dffbd5e4c546eaadacaff72a0db216fdabb5f
Gurkirat Singh <gurkiratsinghofficial@gmail.com>
2020-09-10 23:34:40 +0530

    react skeleton fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react'
 import GurkiratSingh from '../assets/GurkiratSingh.pdf'
 import {AiOutlineInstagram,AiOutlineTwitter } from 'react-icons/ai';
 import {GrFacebookOption} from 'react-icons/gr'
 import { IconContext } from "react-icons";
 import {TiSocialLinkedin} from 'react-icons/ti'
 import {RiGithubLine} from 'react-icons/ri'
-import Skeleton from 'react-loading-skeleton'
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 
-function Home() {
-    return (
-        <div className="home">
-            <div className="row">
-                <div className="column1">
-                    {<img className="logoimg" alt="logo" src={require('../new.png')} /> ||
-                    <Skeleton duration={2} circle={true} height={400} width={400} />}
-
+export class Home extends Component {
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+         image: '',
+        }
+       }
+       handleImageLoaded = () => {
+        this.setState({ image: 'loaded' })
+    }
+    render() {
+        
+        return (
+            <div className="home">
+                <div className="row">
+                    <div className="column1">
+                    {!this.state.image &&
+                    <SkeletonTheme color="#000000" highlightColor="#343A40">
+                    <Skeleton  circle={true} height={350} width={350}/>
+                    </SkeletonTheme>
+   }
+                        <img className="logoimg" alt="logo" src={require('../new.png')} onLoad={this.handleImageLoaded.bind(this)} />
+                        
+    
+                    </div>
+                    <div className="column2">
+                    <h1 >I am Gurkirat Singh</h1>
+                <p >    Technologically savvy and goal oriented. Driven and motivated to help organisations
+                        thrive. Skilled in prioritizing and completeing tasks independently. Good problem solving
+                        skills and attention to details.
+                </p>
+            <div >
+              
+              <p>
+                  <h1><IconContext.Provider value={{ className: "socialIcon" }}>
+                     <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/GurkiratSinghOfficial/"> <GrFacebookOption/></a>
+                     <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/gurkiratsinghofficial/"><AiOutlineInstagram/></a>
+                     <a rel="noopener noreferrer" target="_blank" href="https://github.com/gurkiratsinghofficial"><RiGithubLine /></a>
+                     <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/gurkiratsingho/"><TiSocialLinkedin/></a>
+                     <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/gurkiratsingho"><AiOutlineTwitter/></a>
+                      </IconContext.Provider>
+                    </h1>
+                </p>
+                <p>
+                 <a href={GurkiratSingh}  download><button className="resume">Curriculum vitae</button></a>
+              </p>
+           </div>
+                    </div>
                 </div>
-                <div className="column2">
-                <h1 >I am Gurkirat Singh</h1>
-            <p > Technologically savvy and goal oriented. Driven and motivated to help organisations
-thrive. Skilled in prioritizing and completeing tasks independently. Good problem solving
-skills and attention to details.</p>
-        <div >
-          
-          <p>
-              <h1><IconContext.Provider value={{ className: "socialIcon" }}>
-                 <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/GurkiratSinghOfficial/"> <GrFacebookOption/></a>
-                 <a rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/gurkiratsinghofficial/"><AiOutlineInstagram/></a>
-                 <a rel="noopener noreferrer" target="_blank" href="https://github.com/gurkiratsinghofficial"><RiGithubLine /></a>
-                 <a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/gurkiratsingho/"><TiSocialLinkedin/></a>
-                 <a rel="noopener noreferrer" target="_blank" href="https://twitter.com/gurkiratsingho"><AiOutlineTwitter/></a>
-                  </IconContext.Provider>
-                </h1>
-            </p>
-            <p>
-             <a href={GurkiratSingh}  download><button className="resume">Curriculum vitae</button></a>
-          </p>
-       </div>
-                </div>
+                
             </div>
-            
-        </div>
-    )
+        )
+    }
 }
 
 export default Home
